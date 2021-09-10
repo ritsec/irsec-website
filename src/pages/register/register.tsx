13223303e5ecabedfd57786396570d6ac609f8dc
BradHacker <harkerbd@gmail.com>
2021-09-10 12:21:45 -0400

    Add white team signups and blue team shirts

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -10,7 +10,17 @@ const Register: React.FC = (): React.ReactElement => {
       style={{ backgroundImage: `linear-gradient(to bottom, rgba(18, 18, 18, 1), rgba(0, 0, 0, 0)), url(${HeroBg})` }}
     >
       <div>
-        <h2>They Closed :(</h2>
+        <h2>Signups</h2>
+        <a href="https://forms.gle/nri2VnXs9YquZ8pk8" target="_blank" rel="noreferrer">
+          Blue Team T-Shirt Size Form
+        </a>
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSdjILCRhKajwt6PHJUJri-SrXC4JgubaECYseCP9m94o-6FHw/viewform?usp=sf_link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          White Team
+        </a>
       </div>
     </div>
   );
