733657f22962fbe88a3370cc2e015fb3dd7545d7
BradHacker <harkerbd@gmail.com>
2022-09-15 11:24:08 -0400

    Update '22 Register TBA

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -9,18 +9,22 @@ const Register: React.FC = (): React.ReactElement => {
       className="home register-container"
       style={{ backgroundImage: `linear-gradient(to bottom, rgba(18, 18, 18, 1), rgba(0, 0, 0, 0)), url(${HeroBg})` }}
     >
-      <div>
+      <div className="column">
         <h2>Signups</h2>
-        <a href="https://forms.gle/nri2VnXs9YquZ8pk8" target="_blank" rel="noreferrer">
-          Blue Team T-Shirt Size Form
-        </a>
-        <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSdjILCRhKajwt6PHJUJri-SrXC4JgubaECYseCP9m94o-6FHw/viewform?usp=sf_link"
-          target="_blank"
-          rel="noreferrer"
-        >
-          White Team
-        </a>
+        <div className="row">
+          <p>Compete in IRSeC (Blue Team)</p>
+          <a className="register-link" href="#" target="_blank" rel="noreferrer">
+            {/* <i className="fas fa-external-link-alt" aria-label="register" /> */}
+            Coming Soon
+          </a>
+        </div>
+        <div className="row">
+          <p>Volunteer at IRSeC (White Team)</p>
+          <a className="register-link" href="#" target="_blank" rel="noreferrer">
+            {/* <i className="fas fa-external-link-alt" aria-label="register" /> */}
+            Coming Soon
+          </a>
+        </div>
       </div>
     </div>
   );
